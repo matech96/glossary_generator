94da3a7833418d433bc9620898e98861b0c07046
matech96 <gangoly96@gmail.com>
2019-08-15 18:00:05 +0200

    cached multi processing

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
+import os
+
 import PyPDF2
 import re
 from collections import defaultdict
 import time
+
+from pdfquery.cache import FileCache
+
 from PreallocatedList import PreallocatedList
 import pdfquery
 import unidecode
 import codecs
 import configparser
+import multiprocessing as mp
+import gc
 
 
 def fnPDF_FindText(pdfDoc, words, offset):
@@ -32,25 +39,31 @@ def fnPDF_FindText(pdfDoc, words, offset):
 def pdfquery_FindText(filenamme, words, offset):
     # xfile : the PDF file in which to look
     # xString : the string to look for
+
+    start_time = time.time()
+    querys = [f'LTTextLineHorizontal:contains("{word}")' for word in words]
     res = defaultdict(lambda: PreallocatedList(1000, int))
-    pdf = pdfquery.PDFQuery(filenamme)
+    pdf = pdfquery.PDFQuery(filenamme, parse_tree_cacher=FileCache("tmp/"))
     page_num = 0
-    # p = Pool(4)
     while True:
         try:
             pdf.load(page_num)
-            # res = p.map(lambda word: pdf.pq(f'LTTextLineHorizontal:contains("{word}")'), words)
-            query_objs = [pdf.pq(f'LTTextLineHorizontal:contains("{word}")') for word in words]
+            query_objs = [pdf.pq(query) for query in querys]
             for query_obj, word in zip(query_objs, words):
                 if query_obj:
                     res[word].append(page_num + offset)
             page_num += 1
-            # if page_num == 30:
-            #     break
-            if page_num % 100 == 0:
+            if page_num % 30 == 0:
+                del pdf
+                collected = gc.collect()
+                print(f"Garbage collector: collected {collected} objects.")
+                pdf = pdfquery.PDFQuery(filenamme, parse_tree_cacher=FileCache("tmp/"))
+                # if page_num % 100 == 0:
                 print(page_num)
+            # break
         except StopIteration:
             break
+    print("--- Batch futási idő: %s másodperc ---" % (time.time() - start_time))
     return res
 
 
@@ -74,8 +87,8 @@ def clean_index_reader():
             words.append(line.strip())
     return words
 
-
 def main():
+    os.makedirs('tmp', exist_ok=True)
     config = configparser.ConfigParser(inline_comment_prefixes=('#', ';'))
     config.read_file(codecs.open("config.txt", "r", "utf8"))
     section = config['PDF']
@@ -88,7 +101,24 @@ def main():
     else:
         words = clean_index_reader()
 
+    print(f'Number of words: {len(words)}')
+    pool = mp.Pool(4)
+    batch = []
+    for word in words:
+        batch.append(word)
+        if len(batch) == 5:
+            # process_word_batch(filename, offset, words)
+            pool.apply_async(process_word_batch, (filename, offset, batch))
+            batch = []
+    # process_word_batch(filename, offset, words)
+    pool.apply_async(process_word_batch, (filename, offset, batch))
     print('-----')
+    pool.close()
+    pool.join()
+    # process_word_batch(filename, offset, words)
+
+
+def process_word_batch(filename, offset, words):
     pdf_query = True
     if not pdf_query:
         pdfFileObj = open(filename, 'rb')
@@ -97,7 +127,8 @@ def main():
         pdfFileObj.close()
     else:
         results = pdfquery_FindText(filename, words, offset)
-    with open('results.txt', 'w') as f:
+    # return results
+    with open('results.txt', 'a') as f:
         for k in results:
             pages = sorted(results[k].tolist())
             if len(pages) != 0:
